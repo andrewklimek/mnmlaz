c86faac131be6cbef91b4c9a526a5c0442af1127
Andrew Klimek <andrew.klimek@gmail.com>
2017-06-05 16:16:52 -0400

    support srcset & minimize

--- mnmlaz.js
+++ mnmlaz.js
@@ -2,20 +2,22 @@ function mnmlaz(){
 	
 	function run(){
 		
-		var e = document.querySelectorAll('img[data-src]'),
-		l=e.length, i=0;
+		var el = document.querySelectorAll('img[data-src]'),
+		l=el.length, i=0;
 		
-		if(l<1){
+		if ( l < 1 ){
 			window.removeEventListener('scroll', run );
 			window.removeEventListener('resize', run );
 		}
 
-		for(;i<l;i++){
+		for ( ; i < l; i++ ){
 	
-			var p = e[i].getClientRects()[0];
-			if ( p && p.top < 2 * window.innerHeight ) {
-				e[i].src = e[i].getAttribute('data-src');
-				e[i].removeAttribute('data-src');
+			var pos = el[i].getClientRects()[0];
+			if ( pos && pos.top < 2 * window.innerHeight ) {
+				el[i].src = el[i].getAttribute('data-src');
+				el[i].removeAttribute('data-src');
+				var sset = el[i].getAttribute('data-srcset')
+				if (s) el[i].srcset = sset;
 			}
 		}
 		
@@ -23,13 +25,20 @@ function mnmlaz(){
 	}
 
 	run();
-	window.addEventListener('scroll', run );
-	window.addEventListener('resize', run );
+	window.addEventListener( 'scroll', run );
+	window.addEventListener( 'resize', run );
 }
 
 // test
-var e = document.querySelectorAll('img');
-for ( i=0; i < e.length; i++ ) { e[i].setAttribute('data-src', e[i].src);e[i].removeAttribute('src');}
+var el = document.querySelectorAll('img');
+for ( i=0; i < el.length; i++ ) {
+	el[i].setAttribute('data-src', el[i].src);
+	el[i].removeAttribute('src');
+	if (el[i].srcset) {
+		el[i].setAttribute('data-srcset', el[i].srcset);
+		el[i].removeAttribute('srcset');
+	}
+}
 
 // implement
 document.addEventListener('DOMContentLoaded', function(){
